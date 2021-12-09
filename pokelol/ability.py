a74bb96ce335704025513fb073ab63bb19a1a1bb
adrien <adrienducourthial@gmail.com>
2021-12-09 11:46:07 +0100

    chg: adding __str__ and __repr__. changing init of def, att and abl

--- pokelol/ability.py
+++ pokelol/ability.py
@@ -5,12 +5,12 @@ from typing import Union
 
 from pokelol.poketools import txt_parser, txt_openner
 from env_loader import ATTAQUE_PATH, DEFENSE_PATH
-import random
 
 
 class Ability:
-    def __init__(self, name: str):
+    def __init__(self, name, config: Union[dict, None]):
         self.name = name
+        self.conf = config
         self.kind = None
         self.a_type = None
         self.cost = None
@@ -19,29 +19,35 @@ class Ability:
     def __call__(self, *args, **kwargs):
         pass
 
+    def __str__(self):
+        return str(self.name)
+
+    def __repr__(self):
+        return str(self.name)
+
 
 class Attack(Ability):
     attack_dict = txt_parser(txt_openner(ATTAQUE_PATH))
 
-    def __init__(self, config: Union[dict, None], name: Union[str, None]):
-        super().__init__(config)
+    def __init__(self, name, config: Union[dict, None]):
+        super().__init__(name, config)
         self.accuracy = None
         self.Attribute2 = None
         self.power = None
         self.target = None
 
-    def calc_succes(self):
+    def calc_damage(self):
         pass
 
-    def calc_damage(self):
+    def calc_succes(self):
         pass
 
 
 class Defence(Ability):
     defence_dict = txt_parser(txt_openner(DEFENSE_PATH))
 
-    def __init__(self, config):
-        super().__init__(config)
+    def __init__(self, name, config: Union[dict, None]):
+        super().__init__(name, config)
         self.heal_range = None
 
     def calc_cost(self):
